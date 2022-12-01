91cf03074ccfe719b01b0a27cee09884afa8a501
Nathan <n47h4n7@gmail.com>
2022-12-01 16:35:43 +1300

    Auto select null-ls for pre save LSP

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,3 +1,16 @@
+local lsp_formatting = function(bufnr)
+  vim.lsp.buf.format({
+    filter = function(client)
+      -- apply whatever logic you want (in this example, we'll only use null-ls)
+      return client.name == "null-ls"
+    end,
+    bufnr = bufnr,
+  })
+end
+
+-- if you want to set up formatting on save, you can use this as a callback
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 require("null-ls").setup({
     sources = {
         require("null-ls").builtins.formatting.prettier,
@@ -7,17 +20,19 @@ require("null-ls").setup({
         require("null-ls").builtins.diagnostics.cspell,
         require("null-ls").builtins.code_actions.eslint,
     },
-    -- you can reuse a shared lspconfig on_attach callback here
+    
+    -- add to your shared on_attach callback
     on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.formatting_sync()
-                end,
-            })
-        end
+      if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = augroup,
+          buffer = bufnr,
+          callback = function()
+            lsp_formatting(bufnr)
+          end,
+        })
+      end
     end,
 })
+
